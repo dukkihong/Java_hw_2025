ea34b02263790957edf8fcbf2f431c3a186ef7ec
dukkihong <127312253+dukkihong@users.noreply.github.com>
2025-11-25 12:20:24 +0900

    Add files via upload

---
+++ 12주차_팀과제/team_assignment/Account.java
@@ -0,0 +1,32 @@
+package team_assignment;
+
+public class Account {
+	
+	private String number;
+	private String name;
+	private int balance;
+	
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+
+}
